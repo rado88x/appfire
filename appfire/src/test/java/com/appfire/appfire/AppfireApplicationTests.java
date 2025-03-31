81efaf7d651e21ad602b37e3dd894d54e18bdd93
Rado <rado88x@gmail.com>
2025-03-31 12:15:00 +0300

    init commit

---
+++ appfire/src/test/java/com/appfire/appfire/AppfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appfire.appfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
